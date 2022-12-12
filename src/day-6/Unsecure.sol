d976bc67dd24dcd94a97576f17ff2118aa3bc33b
SHØ <neila@users.noreply.github.com>
2022-12-12 20:55:03 +0900

    day6

--- src/day-6/Unsecure.sol
+++ src/day-6/Unsecure.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.16;
+// @note バージョン特有のバグあるかも？
+pragma solidity =0.8.17;
 
 contract BBB {
 
@@ -10,6 +11,7 @@ contract BBB {
   uint constant REWARD_RATE = 50;
   // 関数のaddressは適当です
   address constant BBBToken = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+  // @note contructor使わなくて良い？msg.sender変わりうる？
   address owner = msg.sender;
   address[] approvedTokens; /// JPYC, USDC, USDTのみがownerからapproveされます
   address[] whitelist;
@@ -25,6 +27,7 @@ contract BBB {
   }
 
   struct TransferInfo {
+    // @note 後でbool呼んでいて、型が違うのでエラーになる?
     uint isETH;         /// 32 bytes
     uint amount;        /// 32 bytes
     address token;      /// 20 bytes
@@ -39,6 +42,7 @@ contract BBB {
   /// @notice  approvedTokens配列にtokenを使いするために使用します
   /// @dev     ownerだけが実行できます
   function addApprovedTokens(address _token) private {
+    // @audit high owner以外も実行できてしまいそう
     if (msg.sender != owner) revert();
     approvedTokens.push(_token);
   }
@@ -53,6 +57,10 @@ contract BBB {
   function getReward(address token) public view returns (uint reward) {
     uint amount = depositAmt[msg.sender][token].amount;
     uint lastTime = depositAmt[msg.sender][token].lastTime;
+    // @note safeMath
+    // @note block.timestamp
+    // @note logicおかしい？
+    // @audit rewardが預けた時間が短いほど高くなるロジックのため、意図せず短期間で大量のBBBトークンを放出してしまう可能性があるa
     reward = (REWARD_RATE / (block.timestamp - lastTime)) * amount;
   }
 
@@ -61,6 +69,7 @@ contract BBB {
     address[] memory _xxx
   ) private pure returns (bool) {
     uint length = _xxx.length;
+    // @note 初期化必要そう
     for (uint i; i < length; ) {
       if (_token == _xxx[i]) return true;
       unchecked {
@@ -83,14 +92,18 @@ contract BBB {
     if (!_isXXX(_token, whitelist)) revert();
     DepostInfo memory depositInfo;
     TransferInfo memory info = TransferInfo({
+        // @note isEth自己申告制
+        // @note 型が違う
         isETH: _isETH,
-        token: _token;
+        token: _token,
         from: msg.sender,
+        //@note amount check
         amount: _amount,
         to: address(this),
     });
 
     _tokenTransfer(info);
+    //@audit depositを2回呼ぶと1回目の預けたトークンの情報が失われる
     depositInfo.lastTime = uint40(block.timestamp);
     depositInfo.amount = _amount;
     depositAmt[msg.sender][_token] = depositInfo;
@@ -111,10 +124,15 @@ contract BBB {
         to: _to,
     });
     uint canWithdrawAmount = depositAmt[msg.sender][_token].amount;
+    //@audit 預けた金額と同額を引き出せない
     require(_info.amount < canWithdrawAmount, "ERROR");
+    // @note memoryの値なので書き換える必要ない？
     canWithdrawAmount = 0;
-    _tokenTransfer(_info);
+    _tokenTransfer(info);
+    // @audit 悪意がなくても無限に引き落とせそう。storageのamountを書き換えていない
     uint rewardAmount = getReward(_token);
+    // @note safeTransfer
+    // @note reentrancy, getRewardが2回呼べそう
     IERC20(BBBToken).transfer(msg.sender, rewardAmount);
   }
 
@@ -122,10 +140,12 @@ contract BBB {
    *********************************   PRIVATE FUNCTIONS     ***********************************
    *********************************************************************************************/
   function _tokenTransfer(TransferInfo memory _info) private {
+    // @note ETHのチェックが自己申告制なのは気になる
     if (_info.isETH) {
       (bool success, ) = _info.to.call{ value: _info.amount }("");
       require(success, "Failed");
     } else {
+      //@note safeTransferFrom
       IERC20(_info.token).transferFrom(_info.from, _info.to, _info.amount);
     }
   }
